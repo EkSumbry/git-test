bd8d7af26ad2d47e41366776911da1007b6311a7
Ekklesia Sumbry <eksumbry@gmail.com>
2023-06-11 16:24:53 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,14 @@
                     $("#carouselButton").children("i").addClass("fa-pause"); 
                 }
         });
+        
+        $("#loginButton").click(function(){
+            $('#loginModal').modal ("show"); 
+         });
+        
+
+        $("#reserveButton").click(function(){
+            $('#reserveModal').modal ("show"); 
+         });
+
     });
